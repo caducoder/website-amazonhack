153fd55be44350a996d90df29096d08edcb25be1
caducoder <cadudrop@hotmail.com>
2023-10-12 23:34:31 -0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h1 className='font-bold text-2xl'>AmazonHack</h1>
+    </main>
+  )
+}
