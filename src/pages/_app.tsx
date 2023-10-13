b7292bfe3ebcb3d229e5386734cee4e7b28b1df9
caducoder <cadudrop@hotmail.com>
2023-10-13 20:35:26 -0300

    fix: style overflow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
